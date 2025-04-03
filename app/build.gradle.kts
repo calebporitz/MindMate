881a1b8ee8b372b208b32d576b82e672c6f2cfcc
Proyolo-ks1 <95938542+Proyolo-ks1@users.noreply.github.com>
2025-04-03 20:06:25 +0200

    very important typo fix in app\build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,7 @@ dependencies {
     implementation(libs.firebase.auth)
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("com.google.android.material:matrial:1.9.0")
+    implementation("com.google.android.material:material:1.9.0")
     implementation("com.google.firebase:firebase-firestore:24.10.0")
     implementation("com.google.firebase:firebase-storage:20.2.1")
     implementation("com.google.firebase:firebase-database:20.1.0")
